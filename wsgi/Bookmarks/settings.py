7ae46d57cc797aa1b664dfaebd555681b98b60a5
Linqing Lu <lilu@redhat.com>
2012-02-27 15:00:12 +0800

    1

--- wsgi/Bookmarks/settings.py
+++ wsgi/Bookmarks/settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'BookMarks.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'BookMarks.bookmarks',
+    'bookmarks',
     'django.contrib.comments',
 )
 
